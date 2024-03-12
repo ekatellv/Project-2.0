9a0710acbe53a702b0966af9517cbd49177b6fcb
shcherbinaE <159794935+shcherbinaE@users.noreply.github.com>
2024-03-13 02:01:16 +0700

    Update loan.py

--- loan.py
+++ loan.py
@@ -1,44 +1,83 @@
-# Part of a case-study #2: Credits
-# Developers: Loseva Ekaterina, Shcherbina Ekaterina
+# Part of a case-study #3: Taxes rate
+# Developers: Loseva Ekaterina, Shcherbina Ekaterina, Nesterova Alexandra
 #
 
 import ru_local as ru
 
-def main():
+MAX_MONTH = 12
+
+def free_tax():
     '''
-    Main function.
-    :return: None
+    The function determines the annual tax-free amount.
     '''
+    amount = 0
+    for month in range(1, MAX_MONTH + 1):
+        value = float(input(f'{ru.FREE_TAX} {ru.NAME[month]} [USD]: '))
+        amount += value
+    return amount
 
-    loan_amount = int(input(ru.SUM_LOAN))
-    loan_payment_1 = int(input(ru.F_PAYMENT))
-    loan_payment_2 = int(input(ru.S_PAYMENT))
+def annual_amount():
+    '''
+    The function determines the annual amount.
+    '''
+    amount = 0
+    for month in range(1, MAX_MONTH + 1):
+        value = float(input(f'{ru.INCOME} {ru.NAME [month]} [USD]: ' ))
+        amount += value
+    return amount
 
-    if loan_amount <= 0 or loan_payment_1 <= 0 or loan_payment_2 <= 0 \
-        or loan_payment_1 + loan_payment_2 < loan_amount \
-        or loan_payment_1 ** 2 + (4 * loan_amount * loan_payment_2 ) < 0:
-        print(f'{ru.FALSE_VALUES}')
-    else:
-        # finding solutions to the quadratic equation
-        # interest_coef = 1 + 0,01 * interest_rate
-        interest_coef_1 = (loan_payment_1 + (loan_payment_1 ** 2 + 4 * loan_amount * loan_payment_2) ** 0.5) / (2 * loan_amount)
-        interest_coef_2 = (loan_payment_1 - (loan_payment_1 ** 2 + 4 * loan_amount * loan_payment_2) ** 0.5) / (2 * loan_amount)
-        interest_rate = round((max(interest_coef_1, interest_coef_2) - 1) * 100, 2)
+def one_subject(amount):
+    percents = [0.1, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396]
+    thresholds = [0, 9075, 36900, 89350, 186350, 405100, 406750]
+    max_threshold = 0
+    while amount > thresholds[max_threshold + 1]:
+        max_threshold += 1
+    taxes = (amount - thresholds[max_threshold]) * percents[max_threshold]
+    for i in range(0, max_threshold):
+        taxes += percents[i] * (thresholds[i + 1] - thresholds[i])
+    return taxes
 
-        print(f'{ru.INTEREST}, {interest_rate}')
+def married_couple(amount):
+    percents = [0.1, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396]
+    thresholds = [0, 18151, 73800, 148850, 226850, 405100, 457600]
+    max_threshold = 0
+    while amount > thresholds[max_threshold + 1]:
+        max_threshold += 1
+    taxes = (amount - thresholds[max_threshold]) * percents[max_threshold]
+    for i in range(0, max_threshold):
+        taxes += percents[i] * (thresholds[i + 1] - thresholds[i])
+    return taxes
 
-        loan_payment_max = int(input(ru.MAX_PAYMENT))
+def single_parent(amount):
+    percents = [0.1, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396]
+    thresholds = [0, 12950, 49400, 127550, 206600, 405100, 432200]
+    max_threshold = 0
+    while amount > thresholds[max_threshold + 1]:
+        max_threshold += 1
+    taxes = (amount - thresholds[max_threshold]) * percents[max_threshold]
+    for i in range(0, max_threshold):
+        taxes += percents[i] * (thresholds[i + 1] - thresholds[i])
+    return taxes
 
-        if loan_payment_max <= 0 or loan_payment_max > loan_amount:
-            print(f'{ru.FALSE_VALUES}')
-        else:
-            loan_term = 0
-            while loan_amount > 0:
-                loan_amount = loan_amount * (1 + 0.01 * interest_rate) - loan_payment_max
-                loan_term += 1
-
-            print(f'{ru.MIN_YEARS}, {loan_term}')
-
+print(f'{ru.TAX_PAYER}')
+category = int(input(f'{ru.VALUES}'))
+print(f'{ru.YEAR_INCOME}')
+amount = annual_amount()
+print(f'{ru.SUM_INCOME}', amount)
+print(f'{ru.FREE_TAX2}')
+free_amount = free_tax()
+print(f'{ru.SUM_FREE_TAX}', free_amount)
+amount -= free_amount
+print(f'{ru.SUM_TAX}', amount)
+match category:
+    case 1:
+        tax = one_subject(amount)
+    case 2:
+        tax = married_couple(amount)
+    case 3:
+        tax = single_parent(amount)
+print(f'{ru.YEAR_TAX}', tax)
+print(f'{ru.MONTH_TAX}', round(tax/12, 2))
 
 if __name__ == '__main__':
     main()
